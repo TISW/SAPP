9d765c65699afa45ddcdeeedee52a080d6ae7d8e
pablomorales01 <pamoral@alumnos.ubiobio.cl>
2014-05-27 00:58:47 -0400

    textarea
    cambio en layout alumno, agregación área de texto

--- protected/views/bitacora/agregar.php
+++ protected/views/bitacora/agregar.php
@@ -14,5 +14,15 @@ $this->breadcrumbs=array(
 <!-- Si es Alumno puede agregar bitácoras-->
 
 <?php if (Yii::app()->user->name == 'alumno') echo '
-<h1>Página Bitácoras Alumno</h1>'
-?>
+<h1>Página Bitácoras Alumno</h1>'?>
+
+
+
+<div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">Página Bitácoras Alumnos</h3>
+      </div>
+      <div class="panel-body">
+        <textarea class="form-control" rows="5" name="User[username]" id="User_username"></textarea>
+      </div>
+    </div>
