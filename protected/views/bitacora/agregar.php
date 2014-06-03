80f2a253c057310ad6c028b4a3544fe5a5ae9847
pablomorales01 <pamoral@alumnos.ubiobio.cl>
2014-06-02 23:21:35 -0400

    validar campos de texto
    para números, letras y caracteres especiales

--- protected/views/bitacora/agregar.php
+++ protected/views/bitacora/agregar.php
@@ -7,6 +7,13 @@ $form = $this->beginWidget('bootstrap.widgets.BsActiveForm', array(
     )
 ));
 
+Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/validCampoFranz.js',CClientScript::POS_END);
+Yii::app()->clientScript->registerScript('validarCamposEspeciales', "
+  $('#Bitacora_BIT_TITULO').validCampoFranz(' abcdefghijklmnñopqrstuvwxyzáéíóú1234567890-');
+  $('#Bitacora_BIT_CONTENIDO').validCampoFranz(' abcdefghijklmnñopqrstuvwxyzáéíóú1234567890-%;:,@().');
+
+");
+
 $this->breadcrumbs=array(
 	'Bitacora'=>array('/bitacora'),
 	'Agregar',
@@ -33,9 +40,9 @@ $this->breadcrumbs=array(
       <table>
       <div class="panel-body">
 
-      <?php echo $form->textFieldControlGroup($model, 'BIT_TITULO', array('placeholder' => 'Bitacora 28-05-14'));?>
+      <?php echo $form->textField($model, 'BIT_TITULO', array('placeholder' => 'Se puede usar: Números, letras y guiones. Ej: Bitácora 02-06-2014', 'prepend'=>'Título*'));?>
       
-      <?php echo $form->textArea($model, 'BIT_CONTENIDO');?> <!-- AREA DE TEXTO-->
+      <?php echo $form->textArea($model, 'BIT_CONTENIDO', array('placeholder' => 'Se puede usar: Números, letras, %, -, @, (). Ej: Hoy 02 de junio avance un 20% de mi práctica', 'prepend'=> 'Contenido' ));?> <!-- AREA DE TEXTO-->
       <br>
       <?php echo BsHtml::submitButton('Enviar', array('color' => BsHtml::BUTTON_COLOR_PRIMARY));?>
 
